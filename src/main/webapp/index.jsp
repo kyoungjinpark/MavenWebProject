e9b5be3fce02f2a872f33acbfe5962dc34afa31e
goodee <goodee@DESKTOP-0RK5HNL>
2018-04-20 11:02:28 +0900

    test 1

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,10 +2,24 @@
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-<body>
-<h2>Hello World! Hello World!</h2>
-<p>내용추가...</p>
-<p>배치 프로그램 확인용...</p>
-<p>Jenkins 배포 확인용...</p>
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>Servlet</title>
+	</head>
+	<body>
+		<h1 style="text-align: center;">환영합니다.</h1>
+		<a href="Calc">Calculator</a>
+		<form action="Calc">
+			<select name="key">
+				<option value="add">덧셈</option>
+				<option value="subtract">뺄셈</option>
+				<option value="multiply">곱셈</option>
+				<option value="divide">나눗셈</option>
+			</select>
+			<input type="number"  name="a" placeholder="첫번째 숫자를 입력하세요">
+			<input name="b" placeholder="두번째 숫자를 입력하세요">
+			<input type="submit" value="계산">
+		</form>	
+		
+	</body>
 </html>
---
+++ src/test/java/kr/co/gdu/CalculatorTest.java
@@ -0,0 +1,46 @@
+package kr.co.gdu;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	Calculator calc;
+	int a, b ;		
+	
+	
+		@Before
+		public void before() {
+			System.out.println("before()");
+			calc = new Calculator();
+			a = 2;
+			b = 6;
+		}
+		
+		@Test
+		public void test() {
+			System.out.println("test()");
+			int add = calc.add(a, b); // 2 + 6 = 8
+			int subtract = calc.subtract(add, b); // 8 - 6 = 2
+			int multiply = calc.multiply(subtract, a); // 2 * 2 = 4
+			int divide = calc.divide(multiply, a); // 4 * 2 = 2
+			assertEquals(2,  divide);
+			
+		}
+		
+		@After
+		public void after() {
+			System.out.println("after()");
+		}
+		
+		
+	}
+
+	
+	
+	
+
+
